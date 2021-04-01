13665621a8b340f602d41b6f420ddf6c5659c6d4
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-04-01 17:32:51 -0400

    Update ApplyingFilters.cpp

--- ApplyingFilters.cpp
+++ ApplyingFilters.cpp
@@ -13,26 +13,26 @@ Mat dst;
 int main() {
 	namedWindow(window_name, WINDOW_AUTOSIZE);
 
-    std::string image_path = samples::findFile("amg.jpg");
-    Mat src= imread(image_path, IMREAD_COLOR);
-    //clones the source image
+	std::string image_path = samples::findFile("amg.jpg");
+	Mat src= imread(image_path, IMREAD_COLOR);
+	//clones the source image
 
-    dst = src.clone();
-    imshow(window_name, dst);
-    waitKey(0);
-    
-    //blurring
+	dst = src.clone();
+	imshow(window_name, dst);
+	waitKey(0);
 
-    blur(src, dst, Size(MAX_KERNEL_LENGTH, MAX_KERNEL_LENGTH) , Point(-1, -1));
-    imshow(window_name, dst);
-    waitKey(0);
+	//blurring
 
-    //Gaussian blurring
+	blur(src, dst, Size(MAX_KERNEL_LENGTH, MAX_KERNEL_LENGTH) , Point(-1, -1));
+	imshow(window_name, dst);
+	waitKey(0);
 
-    GaussianBlur(src, dst, Size(MAX_KERNEL_LENGTH, MAX_KERNEL_LENGTH), 0, 0);
-    imshow(window_name, dst);
-    waitKey(0);
+	//Gaussian blurring
 
-    return 0;
+	GaussianBlur(src, dst, Size(MAX_KERNEL_LENGTH, MAX_KERNEL_LENGTH), 0, 0);
+	imshow(window_name, dst);
+	waitKey(0);
+
+	return 0;
 
 }
