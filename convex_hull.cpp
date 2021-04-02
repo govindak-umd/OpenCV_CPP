7ba9c2eb1b94a8030897aa3d8ea4545681eb5288
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-04-02 12:13:06 -0400

    Update convex_hull.cpp

--- convex_hull.cpp
+++ convex_hull.cpp
@@ -4,9 +4,18 @@
 #include <iostream>
 
 
+cv::Mat src;
+cv::Mat src_grayed;
+cv::RNG rng(1233);
+
+int thresh = 0;
+
+void thresh_callback(int, void*);
+
 int main() {
 
-	cv::Mat src = cv::imread("hands.jpg", cv::IMREAD_COLOR);
+	// src = cv::imread("hands.jpg", cv::IMREAD_COLOR);
+	src = cv::imread("amg.jpg", cv::IMREAD_COLOR);
 
 	cv::Mat src_grayed;
 	cv::RNG rng(1233);
@@ -20,9 +29,39 @@ int main() {
 	cv::blur(src_grayed, src_grayed, cv::Size(5, 5));
 
 	cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
-
 	cv::imshow("Image", src);
-	cv::waitKey(0);
+
+	
+	const int max_thresh = 255;
+	cv::createTrackbar("Canny threshold", "Image", &thresh, max_thresh, thresh_callback);
+	thresh_callback(0, 0);
+
+	cv::waitKey();
 
 	return 0;
 }
+
+void thresh_callback(int, void*) {
+	cv::Mat canny_output;
+	cv::Canny(src_grayed, canny_output, thresh, thresh*2);
+
+	std::vector<std::vector<cv::Point>> contours;
+
+	cv::findContours(canny_output, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+	std::vector<std::vector<cv::Point>> convex_hull_pts(contours.size());
+	
+	for (size_t i = 0; i < contours.size(); i++) {
+		cv::convexHull(contours[i], convex_hull_pts[i]);
+	}
+
+	cv::Mat drawing = cv::Mat::zeros(canny_output.size(), CV_8UC3);
+
+	for (int i = 0; i < contours.size(); i++) {
+		cv::drawContours(drawing, contours, i, cv::Scalar(0, 0, 255));
+		cv::drawContours(drawing, convex_hull_pts, i, cv::Scalar(255, 0, 0));
+	}
+
+	cv::imshow("Convex Hull Drawing", drawing);
+
+}	
