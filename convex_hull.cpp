78716f201a6efb6d0a5118e8e2851a29f491c6be
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-04-02 11:50:42 -0400

    Conex hull, in progress

--- convex_hull.cpp
+++ convex_hull.cpp
@@ -2,3 +2,27 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
+
+
+int main() {
+
+	cv::Mat src = cv::imread("hands.jpg", cv::IMREAD_COLOR);
+
+	cv::Mat src_grayed;
+	cv::RNG rng(1233);
+
+
+	// gray the image
+	cv::cvtColor(src, src_grayed, cv::COLOR_BGR2GRAY);
+
+	// blur the image
+
+	cv::blur(src_grayed, src_grayed, cv::Size(5, 5));
+
+	cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
+
+	cv::imshow("Image", src);
+	cv::waitKey(0);
+
+	return 0;
+}
